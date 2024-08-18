2adb60f413ee94d9d577e7a798916a3755e697a3
AdnanNasifCloud <joseph.nasif@gmail.com>
2024-08-18 15:26:33 +0000

    1

---
+++ jellyfin/config/data/ScheduledTasks/3a025083-141d-3c17-dd96-d5f9b951287b.js
@@ -0,0 +1,1 @@
+{"StartTimeUtc":"2024-08-18T15:25:00.0142773Z","EndTimeUtc":"2024-08-18T15:25:00.0195126Z","Status":"Completed","Name":"Clean up collections and playlists","Key":"CleanCollectionsAndPlaylists","Id":"3a025083141d3c17dd96d5f9b951287b"}
